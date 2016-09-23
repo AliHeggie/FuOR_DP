1bfd8c9d7b74017c05c087a1f7f9975563ba51a9
HEGGIE Alastair <s0822514@ed.ac.uk>
2016-09-23 16:46:27 +0100

    First go at writing this program
    Simple and quite inneficient version

--- DP_python/ex2_pumps.py
+++ DP_python/ex2_pumps.py
@@ -1,9 +1,24 @@
 # Excercise 2 from FuOR tutorial 1
 
-def recPumps(Demands,ProductionCosts):#,CurrentInventory,InventoryCost,InventoryLimit):
-   if len(Demands) <=1:
-        cost = ProductionCosts[Demands[0]]
-   return cost
+def recPumps(Demands,ProductionCosts,Penalty,CurrentInventory):#,CurrentInventory,InventoryCost,InventoryLimit):
+    mincost = Penalty
+    if len(Demands) <=1:
+        if Demands[0]-CurrentInventory < len(ProductionCosts):
+            mincost = ProductionCosts[Demands[0]-CurrentInventory]
+    else:
+        for p in range(len(ProductionCosts)):
+            cost = ProductionCosts[p] + recPumps(Demands[1:],ProductionCosts,Penalty,max(CurrentInventory+p-Demands[0],0))
+            if CurrentInventory + p < Demands[0]:
+                cost += Penalty
+            if cost < mincost:
+                mincost = cost
+    return mincost
 
+CurrentInventory = 0
+Demands = [1,3,2,1]
+#Demands = [1,1,2,3,4,1,3,4,5,1,4,5] #This breaks the program as the size of the problem explodes
+ProductionCosts = [0,1,3,6,12]
+Penalty = 999
 
-print(recPumps([2],[0,1,3,5]))
+print(recPumps(Demands,ProductionCosts,Penalty,CurrentInventory))
+
