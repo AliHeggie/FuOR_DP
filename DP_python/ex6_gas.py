5110f908140546cb470fb8181f88dcee04226d18
HEGGIE Alastair <s0822514@ed.ac.uk>
2016-09-25 15:09:51 +0100

    First go at Exercise 6
    Simple code to solve exercise 6. Need to check for bugs

---
+++ DP_python/ex6_gas.py
@@ -0,0 +1,21 @@
+# Excercise 2 from FuOR tutorial 1
+from __future__ import division
+
+def recGas(Price,CurrentVolume,ExtractionLimit):#,CurrentInventory,InventoryCost,InventoryLimit):
+    MaxProfit = 0
+    if len(Price) <=1:
+        MaxProfit = Price[0]*CurrentVolume*ExtractionLimit
+    else:
+        ProfitExtractAll = Price[0]*CurrentVolume*ExtractionLimit + recGas(Price[1:],CurrentVolume-(CurrentVolume*ExtractionLimit),ExtractionLimit)
+        ProfitExtractNothing = recGas(Price[1:],CurrentVolume,ExtractionLimit)
+        if ProfitExtractAll < ProfitExtractNothing:
+             MaxProfit = ProfitExtractNothing
+        else:
+             MaxProfit = ProfitExtractAll
+    return MaxProfit
+
+Price = [1,2]
+InitalVolume = 8
+ExtractionLimit = 1/2
+
+print(recGas(Price,InitalVolume,ExtractionLimit))
