ea1fe00fa154125596cee426a4fc3398a2e2cbf3
HEGGIE Alastair <s0822514@ed.ac.uk>
2016-09-27 13:33:22 +0100

    Bugfix wrong answer
    Value of gas left in well was incorrect due to still applying extraction
    limit. Closes #2

--- DP_python/ex6_gas.py
+++ DP_python/ex6_gas.py
@@ -4,7 +4,7 @@ from __future__ import division
 def recGas(Price,CurrentVolume,ExtractionLimit):#,CurrentInventory,InventoryCost,InventoryLimit):
     MaxProfit = 0
     if len(Price) <=1:
-        MaxProfit = Price[0]*CurrentVolume*ExtractionLimit
+        MaxProfit = Price[0]*CurrentVolume
     else:
         ProfitExtractAll = Price[0]*CurrentVolume*ExtractionLimit + recGas(Price[1:],CurrentVolume-(CurrentVolume*ExtractionLimit),ExtractionLimit)
         ProfitExtractNothing = recGas(Price[1:],CurrentVolume,ExtractionLimit)
@@ -14,7 +14,7 @@ def recGas(Price,CurrentVolume,ExtractionLimit):#,CurrentInventory,InventoryCost
              MaxProfit = ProfitExtractAll
     return MaxProfit
 
-Price = [1,2]
+Price = [12,15,20,11,8]
 InitalVolume = 8
 ExtractionLimit = 1/2
 
